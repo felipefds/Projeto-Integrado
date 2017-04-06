2cdcd0f616e3ada2a55dd9798cb2b68e71230609
Felipe Ferreira <felipeferreira@poli.ufrj.br>
2017-04-05 23:11:55 -0300

    O codigo em arduino agora divide a string recebida via seria em 4 diferentes strings, correspondentes aos quatro angulos e as converte para inteiro, ao passa-las como argumento para s.write

--- ard_mainProgram_v2.ino
+++ ard_mainProgram_v2.ino
@@ -1,130 +1,56 @@
 #include <string.h>
-#include <Servo.h>
+#include <stdlib.h>
+//#include <Servo.h>
 
-#define MAX_CARACTERS 100
-#define SERVO   6
+#define MAX_CARACTERS    100
+#define MAX_ORDEM_ANGULO 3
 
-int PinAnalogLM35 = 0; //Setando Pino A0
-float valAnalog = 0; // Iniciando variavel valAnalog como 0
-float temp = 0; //Iniciando variavel temp como 0
 unsigned count = 0;
-unsigned aux, micNumbers, i, j, k;
-char **validacao;
-Servo s;
+unsigned positionMic = 1;
+//Servo s;
 
-char subtexts [10][7];
-
-boolean stopBit;
-char received [MAX_CARACTERS];  
 char name [MAX_CARACTERS];
-
-struct Mic {
-  char text[7];
-  char label[10];
-  char angleString[3];
-  unsigned long angle;
-};
-
-Mic micSet [10];
+char anguloNorte [MAX_ORDEM_ANGULO];
+char anguloLeste [MAX_ORDEM_ANGULO];
+char anguloSul [MAX_ORDEM_ANGULO];
+char anguloOeste [MAX_ORDEM_ANGULO];
 
 void setup()
 {
- s.attach(SERVO);
+ //s.attach(SERVO);
  Serial.begin(9600);
- s.write(0);
+ //s.write(0);
 }
- 
 
- 
-void loop()
-{
+void loop() { 
 
- stopBit = false;
- 
- if (Serial.available() > 0)
- {
-  
-      received[0] = Serial.read();
-      name [count] = received[0];
-      count++;
-      Serial.println (name);  
-      if (received[0] == '.')
-        stopBit = true;
-      
- }
-  if (stopBit == true)
-  {
-    Serial.println (name[1]);
-    stopBit = false;
-    count = 0;
-    i = 0;
-    j = 0;
-  
-    for (k = 3; name[k] != '.'; k++)
-    {
+  while (Serial.available() > 0) {
+    name[count] = Serial.read();
 
-      if (name[k] != ',')
-      {
-        subtexts [i][j] = name[k];
-        j++;
+    if (name[count] != ';'){
+      if (positionMic == 1){
+        anguloNorte[count] = Serial.read();
+        count++;
       }
-      else
-      {
-        j = 0;
-        i++;
+      if (positionMic == 2){
+        anguloLeste[count] = Serial.read();
+        count++;
       }
+      if (positionMic == 3){
+        anguloSul[count] = Serial.read();
+        count++;
+      }
+      if (positionMic == 4){
+        anguloOeste[count] = Serial.read();
+        count++;
+      }
+    } else {
+      positionMic++;
+      count = 0;
     }
-    for (i = 0; i < name[1]-'0'; i++)
-    {
-      strcpy(micSet[i].text,subtexts[i]);
-      //Serial.println(subtexts[i]);
-      Serial.println(micSet[i].text);
-    }
-    
-    for (i = 0; i < name[1]-'0';i++)
-    {
-      for (j = 0; micSet[i].text[j] != '['; j++)
-        micSet[i].label[j] = micSet[i].text[j];
-    
-      for (j++, k = 0; micSet[i].text[j] != ']'; j++, k++)
-        micSet[i].angleString[k] = micSet[i].text[j];
-     
-
-      
-      Serial.print(micSet[i].label);
-      Serial.print("\t-\t");
-      Serial.print(micSet[i].angleString);
-      Serial.print("\t-\t");
-      micSet[i].angle = strtoul(micSet[i].angleString, validacao, 10);
-      Serial.println(micSet[i].angle);
-      //micSet[i].angle = atoi (micSet[i].angleString);
-//      micSet[i].angle = (micSet[i].angleString.parseInt());
-      //Serial.println(micSet[i].angle);
-       
-    }
-    
-  }
-  
-  
-  s.write(micSet[4].angle);
-
-  
-  
-//      Serial.println (name);   
- /*
- if (Serial.available() == 0)
- {
-   aux = 0;
-   micNumbers = 0;
-   while (received[aux] != '.')
-   {
-     if ((received[aux]==',') & (received[aux+1]!=',' & received[aux+1]!='.'))
-     {
-       micNumbers++;
-       Serial.println(micNumbers);
-     }
-     
-   }
- }
-  */
-}
+  } //Fim da Leitura Serial
+  s.write(strtol(anguloNorte));
+  s.write(strtol(anguloLeste));
+  s.write(strtol(anguloSul));
+  s.write(strtol(anguloOeste));
+} //Fim do loop
